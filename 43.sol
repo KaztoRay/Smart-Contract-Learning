90d9e8f7ebf899a2c959a7288409c4b01126ac2e
kaztoray <dsph9245@naver.com>
2025-10-27 09:29:41 +0900

    master

---
+++ 43.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract ValueTypes {
+
+    uint128 v0;
+    uint64 v1;
+    uint32 v2;
+    uint16 v3;
+    uint8 v4;
+
+    constructor() {
+
+        v0 = 0x1337;
+        v2 = 0x337;
+        v4 = 0x37;
+
+    }
+
+}
