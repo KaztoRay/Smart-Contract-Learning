59cc05ff4dd977f68ed35dde3c3cc7a3666bb1ef
kaztoray <dsph9245@naver.com>
2025-10-28 09:32:39 +0900

    master

---
+++ 51.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Reentrancy {
+
+    mapping(address => uint) balance;
+
+    // ...
+
+    function withdraw(uint amount) external {
+
+        if (balance[msg.sender] >= amount) {
+
+            (bool result, ) = msg.sender.call{value: amount}
+            require(result, "Call failed");
+            balance[msg.sender] -= amount;
+
+        }
+
+    }
+
+}
