cea409d6a7d155acf574b3bc70e172a86a93c0c7
kaztoray <dsph9245@naver.com>
2025-10-28 19:05:46 +0900

    master

---
+++ 55.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.6.0;
+
+contract Test {
+
+    receive() external payable {
+
+        // React to receiving Ether
+
+    }
+
+    fallback() external payable {
+
+        ...
+
+    }
+    
+}
---
+++ 56.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Test {
+
+    fallback(bytes calldata x) external payable returns (bytes memory) {
+
+        ...
+
+    }
+
+}
